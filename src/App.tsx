62192186a2ae575949afaf876d2367a42e70181b
Can Soylu <hsyncn17@gmail.com>
2021-09-24 02:28:45 +0300

    useContext future value

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import "./App.css";
-import Box from "./components/context/Box";
-import { ThemeContextProvider } from "./components/context/ThemeContext";
+import { UserContextProvider } from "./components/context/UserContext";
+import User from "./components/context/User";
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <Box />
-      </ThemeContextProvider>
+      <UserContextProvider>
+        <User />
+      </UserContextProvider>
     </div>
   );
 }
