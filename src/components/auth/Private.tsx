4291e88d241053cdb26dec0a095c4915bc872679
Can Soylu <hsyncn17@gmail.com>
2021-09-24 13:52:09 +0300

    component prop type

---
+++ src/components/auth/Private.tsx
@@ -0,0 +1,18 @@
+import Login from "./Login";
+
+import { ProfileProps } from "./Profile";
+
+type PrivateProps = {
+  isLoggedIn: boolean;
+  component: React.ComponentType<ProfileProps>;
+};
+
+const Private = ({ isLoggedIn, component: Component }: PrivateProps) => {
+  if (isLoggedIn) {
+    return <Component name="can" />;
+  } else {
+    return <Login />;
+  }
+};
+
+export default Private;
---
+++ src/components/auth/Profile.tsx
@@ -0,0 +1,9 @@
+export type ProfileProps = {
+  name: string;
+};
+
+const Profile = ({ name }: ProfileProps) => {
+  return <div>Private profile component {name}</div>;
+};
+
+export default Profile;
